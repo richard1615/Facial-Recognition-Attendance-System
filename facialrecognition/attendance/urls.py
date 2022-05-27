298a2edee81709e5d8ead4902e220f3b30f55385
richard1615 <malavikaha02@gmail.com>
2022-05-27 16:31:34 +0530

    Added urls

---
+++ facialrecognition/attendance/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+    path('addcourse/', views.addcourse, name='addcourse'),
+    path('delcourse/<int:course_id>', views.delcourse, name='delcourse'),
+    path('students/<int:course_id>', views.students, name='students'),
+    path('course/<int:course_id>', views.course, name='course'),
+    path('delstudent/<int:course_id>/<int:student_id>', views.delstudent, name='delstudent'),
+    path('addclass/<int:course_id>', views.addclass, name='addclass'),
+    path('attendance/<int:class_id>', views.attendance, name='attendance'),
+    path('markattendance/<int:class_id>', views.markAttendance, name='markattendance'),
+]
